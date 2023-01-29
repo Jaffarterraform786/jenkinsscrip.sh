f9aeb1faf5b1576de8c82cddda268dc95109ebb9
Adelakun <sollyray01@yahoo.co.uk>
2023-01-29 15:57:23 -0500

    tickets number

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -14,8 +14,9 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
 
+##Extra packages
 # run jenkins
-mkdir -p /var/jenkins_home
+apt-get install vim git -y
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
