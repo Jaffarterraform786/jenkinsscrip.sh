bfe4a0f1742fddee465928b97601cf410b03c43e
Ofori <roforiantwi@yahoo.com>
2023-01-29 15:57:52 -0500

    ticket number

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -13,8 +13,10 @@ apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
-
 # run jenkins
+## Extra packages
+apt-get install vim -y
+apt-get install git -y 
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
