4a1c9f78203e8a095213a4ae19067c0a215d3b57
Sergio <ngoundeserge@gmail.com>
2023-01-29 15:57:30 -0500

    ticket number

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -14,6 +14,10 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
 
+## Extra package
+
+apt-get install vim git -y
+
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
