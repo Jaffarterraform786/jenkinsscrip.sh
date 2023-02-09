3720edfb6ebd526d682a64a7ebe839ae21a8af8e
Mohamed <moctamba1@gmail.com>
2023-02-08 18:58:10 -0600

    changes

--- jenkinsscrip.sh
+++ jenkinsscrip.sh
@@ -21,7 +21,8 @@ apt install vim git -y
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
-
+## Extra package
+apt-get install vim -ys
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'#!/bin/bash
