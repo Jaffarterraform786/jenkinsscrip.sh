2636556dce2e6ceebf8d03c3fa7e66cc1beee9e4
Thelma <thelma.paulison@yahoo.fr>
2023-04-10 16:21:06 +0200

    undo some chages

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -13,10 +13,6 @@ apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
-
-## Extra packages install
-apt install vim git -Y
-
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
